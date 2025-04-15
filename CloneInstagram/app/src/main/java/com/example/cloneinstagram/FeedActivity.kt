df14cdca218d9e0dcb2f62b2849b060ffc4b84a5
Bora Yıldırım <borayldrm@hotmail.com>
2025-04-15 12:08:15 +0300

    create: create new login page update: update codes and libs

--- CloneInstagram/app/src/main/java/com/example/cloneinstagram/FeedActivity.kt
+++ CloneInstagram/app/src/main/java/com/example/cloneinstagram/FeedActivity.kt
@@ -5,23 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.cloneinstagram.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class FeedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-
+        setContentView(R.layout.activity_feed)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-
     }
 }
